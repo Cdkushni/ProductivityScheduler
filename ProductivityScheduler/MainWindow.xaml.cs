9bc8a0ec3d76554aaf81780f13d676181ff62d6e
Colin Kushniruk <colinkushniruk@gmail.com>
2017-08-01 16:12:44 -0600

    Finally got the Property notifications working for the event node classes to notify the listbox of changes.

--- ProductivityScheduler/MainWindow.xaml.cs
+++ ProductivityScheduler/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Collections.ObjectModel;
+
 namespace ProductivityScheduler
 {
     /// <summary>
@@ -20,9 +22,66 @@ namespace ProductivityScheduler
     /// </summary>
     public partial class MainWindow : Window
     {
+        int counter = 0;
+        ScheduleEvent monday = new ScheduleEvent();
+        List<EventNode> items = new List<EventNode>();
+
         public MainWindow()
         {
             InitializeComponent();
+            monday.EventList.Add(new EventNode() { day = "Monday", time = 2 });
+            monday.EventList.Add(new EventNode() { day = "Tuesday", time = 1 });
+            lbMonday.ItemsSource = monday.EventList;
+        }
+
+        private void DataGridWeekInit(object sender, EventArgs e)
+        {
+            monday.EventList.Add(new EventNode() { day = "Wednesday", time = 40 });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.DataGridWeekInit(sender, e);
         }
     }
+
+    public interface IProperty
+    {
+        //TODO: Add changeable node properties here!!!!
+    }
+
+    public class EventNode
+    {
+        
+        public string day { get; set; }
+        public DateTime formatTime { get; set; }
+        public int time { get; set; }
+
+        private ObservableCollection<IProperty> propertyList;
+        public ObservableCollection<IProperty> PropertyList
+        {
+            get
+            {
+                if (propertyList == null)
+                    propertyList = new ObservableCollection<IProperty>();
+                return propertyList;
+            }
+        }
+    }
+
+    public class ScheduleEvent
+    {
+        private ObservableCollection<EventNode> eventList;
+        public ObservableCollection<EventNode> EventList
+        {
+            get
+            {
+                if (eventList == null)
+                    eventList = new ObservableCollection<EventNode>();
+                return eventList;
+            }
+        }
+
+    }
+
 }
